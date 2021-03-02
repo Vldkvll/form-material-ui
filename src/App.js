2ef49442a1a0df963c1ba394fa3f2c6b59758239
Vladyslav Koval <49593840+Vldkvll@users.noreply.github.com>
2021-03-02 02:45:59 +0200

    init app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+
+const Step1 = () => (
+  <>Step1</>
+)
+const Step2 = () => (
+  <>Step2</>
+)
+const Step3 = () => (
+  <>Step3</>
+)
+const Result = () => (
+  <>Result</>
+)
+const Header = () => (
+  <h1>Full React Form</h1>
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+                <Header />
+            <Router>
+              <Switch>
+<Route path='/' exact component={Step1} />
+<Route path='/step2' component={Step2} />
+<Route path='/step3' component={Step} />
+<Route path='/result' component={Result} />
+              </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
